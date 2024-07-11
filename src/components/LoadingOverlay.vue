62f559736ce3586dba6995a56b6ddf41713ba0e8
Pesy Wu <GamerNoTitle@outlook.com>
2024-07-11 16:17:56 +0800

    Update LoadingOverlay.vue

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,8 +1,9 @@
 <template>
 	<div
 		v-if="props.loading"
-		class="backdrop-blur-sm left-0 top-0 h-full w-full absolute flex items-center justify-around z-50 cursor-not-allowed"
+		
 	>
+		<!-- class="backdrop-blur-sm left-0 top-0 h-full w-full absolute flex items-center justify-around z-50 cursor-not-allowed" -->
 		<font-awesome-icon :icon="faSpinner" class="text-4xl text-gray-400 animate-spin" />
 	</div>
 </template>
